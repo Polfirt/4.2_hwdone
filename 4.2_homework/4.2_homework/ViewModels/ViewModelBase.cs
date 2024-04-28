23ad7fdeb6a851a9302e720a903c570bc8440d01
Polfirt <Polfirt@gmail.com>
2024-04-28 18:08:19 +0300

    tt

---
+++ 4.2_homework/4.2_homework/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace _4._2_homework.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ 4.2_homework/4.2_homework/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace _4._2_homework.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
